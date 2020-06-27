6e1f8320f22be83f36e53e6165f457a7111bce37
Tony <xya76@sfu.ca>
2020-06-26 21:42:26 -0700

    Modify Javascript File

--- js/carousel.js
+++ js/carousel.js
@@ -1,4 +1,9 @@
 setup = function(){
 	$(".menu-expand").click(function() {$("nav").slideToggle("slow");})
+	alert("ffff");
+	$(".filter").click(function() {
+		$(".side-menu-top").slideToggle("slow");
+		$(".side-menu-bottom").slideToggle("slow");
+	})
 }
 $(document).ready(setup);
